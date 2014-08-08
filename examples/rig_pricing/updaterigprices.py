91cc23c1631322f28ea8ac2839dcc5aeaac44b83
J.C. Woltz <jwoltz@gmail.com>
2014-08-08 16:37:13 -0400

    fix requests, and display verbose info fixes

--- examples/rig_pricing/updaterigprices.py
+++ examples/rig_pricing/updaterigprices.py
@@ -13,16 +13,17 @@ import mrrapi
 ##############################################################################
 # Change settings here
 debug = False
-verbose = False
+# Verbose displays useful info. Turn off if you do not want it displayed
+verbose = True
 
 # your key and secret
 mkey = 'YourKey'
 msecret = 'YourSecret'
 
 # you device id or list of devices, comma separated
-mrrdevices = [7281]
-ppalgo = 'X11'      #PoolPicker algo
-mrralgo = 'x11'     #MRR Algo Name
+mrrdevices = [6830, 6899]
+ppalgo = 'Scrypt'      # PoolPicker algo: Scrypt, ScryptN, X11, X13, X15
+mrralgo = 'scrypt'     # MRR Algo Name  : scyrpt, scryptn, x11, x13, x15
 # Pools to ignore from poolpicker
 PoolPickerIgnore = ['AltMining.Farm','bobpool']
 ##############################################################################
@@ -45,34 +46,39 @@ def getBTCValue():
 
 def getPoolPickerAlgo(algo='ScryptN'):
     algobtc =[]
-    layout = "{0:>25}{1:>15}"
+    layout = "{0:>45}{1:>15}"
     url = "http://poolpicker.eu/api"
     res = requests.get(url, verify=False)
-    if (os.name == 'nt'):
+    if (requests.__version__.split('.')[0] > 0):    #Detect old version of requests used by distros
         tr = res.json()
     else:
         tr = res.json
+    if verbose:
+        print(layout.format(" Pool Name ", "Payout "))
     for x in tr['pools']:
         if x['profitability'].has_key(algo):
             if x['profitability'][algo][0]['btc']:
                 if str(x['name']) not in PoolPickerIgnore:
                     algobtc.append(float(x['profitability'][algo][0]['btc']))
-                    print(layout.format(str(x['name']),str(round(float(x['profitability'][algo][0]['btc']),8))))
-                else:
-                    #Skip this pool
-                    pass
+                    if verbose:
+                        print(layout.format(str(x['name']),str(round(float(x['profitability'][algo][0]['btc']),8))))
+    if verbose:
+        print(layout.format(" ---------------", "  ----------"))
     if len(algobtc) > 0:
         algobtc.sort()
+        if verbose:
+            print(layout.format(" Max Payout",round(max(algobtc),8) ))
         if debug:
             print algobtc
         return max(algobtc)
     else:
+        print "No Pricing available"
         return 1
 
 def getmrrlow(ignoreFirstrigs=0,min_price=0):
     global mrrrigs, mrrprices
     mrrpp = []
-    # Get a list of rigs above 10MHs and above the max poolpicker payout
+    # Get a list of rigs above 10MHs and above min_price of rig_type
     mrrp = mapi.rig_list(10,0,min_price,rig_type=mrralgo)
     if (str(mrrp['success'] == 'True') and len(mrrp['data']['records']) > 0):   #make sure we have valid data
         for x in mrrp['data']['records']:
@@ -83,7 +89,7 @@ def getmrrlow(ignoreFirstrigs=0,min_price=0):
                     if float(x['rating']) > 4.0:                                #only compare highly rated rigs
                         mrrrigs.update({int(x['id']): {'hashrate': float(x['hashrate']), 'price': float(x['price_mhash'])}})
                         mrrpp.append(float(x['price_mhash']))
-                    elif verbose:
+                    elif debug:
                         print "Ignoring rig: " + str(x['id']) + " With a rating of " + str(x['rating']) + " for " + str(x['name'])
         for e in mrrpp:
             if e not in mrrprices:
@@ -109,20 +115,23 @@ def getmrrlow(ignoreFirstrigs=0,min_price=0):
 def setRigPrice(rigId,setPrice,rigDetail=None):
     if rigDetail is not None:
         if float(rigDetail['data']['price']) != float(setPrice):
-            print "Changing rig price from: " + str(float(rigDetail['data']['price'])) + " to " + str(setPrice)
+            if verbose:
+                print "Changing rig price from: " + str(float(rigDetail['data']['price'])) + " to " + str(setPrice)
             mapi.rig_update(str(rigDetail['data']['id']),price=str(setPrice))
         else:
             if verbose:
                 print "Rig " + str(rigDetail['data']['id']) + " already at " + str(setPrice)
     else:
-        print "Setting Rig " + str(rigId) + " price: " + str(setPrice)
+        if verbose:
+            print "Setting Rig " + str(rigId) + " price: " + str(setPrice)
         mapi.rig_update(str(rigId),price=str(setPrice))
 
 def updatemyRigsPrices(percenta,percentr,setPrice,ppPrice):
     """
     :param percenta: Multiplier for max PoolPicker Price when rig available
     :param percentr: Multiplier for max PoolPicker Price when rig is rented
-    :param setPrice: Lowest setPrice to set rig
+    :param setPrice: Lowest setPrice to set rig. Lowest rate you will accept
+    :param ppPrice: Price from poolpicker or other function. this will be multiplied by percent a and r
     :return:
     """
     for x in mrrdevices:
@@ -151,15 +160,22 @@ def calculateMaxIncomeA():
     rentalfee = float(0.035)
     outcome = float(0)
     mhash = float(0)
-    layout = "{0:>65}{1:>10}{2:>10}{3:>15}{4:>12}"
-    print(layout.format("  Device Name  ", " Speed","Price", "Daily income", "Rented?"))
+    layout = "{0:>65}{1:>10}{2:>10}{3:>15}{4:>14}"
+    print(layout.format("  Device Name  ", " Speed ","Price  ", "Daily income", "Rented? "))
     for x in mrrdevices:
         t = mapi.rig_detail(x)
+        rigstat = "available"
         if (t['success'] == True):
             mhashrate = float(float(t['data']['hashrate']['advertised'])/(1000000.0))
             mhash += mhashrate
             dailyprice = mhashrate * float(t['data']['price']) * (1.0 - rentalfee)
-            print(layout.format(str(t['data']['name']),str(mhashrate) + " MH",str(round(float(t['data']['price']),6)) ,str(dailyprice), str(t['data']['status'])))
+            if (str(t['data']['status']) == 'rented'):
+                aih = float(t['data']['available_in_hours'])
+                rigstat = "R "
+                if 0.1 < aih < 10.0:
+                    rigstat += " "
+                rigstat += str(aih) + " hrs"
+            print(layout.format(str(t['data']['name']),str(mhashrate) + " MH",str(round(float(t['data']['price']),6)) ,str(round(dailyprice,8)), rigstat))
             outcome += dailyprice
 
 def printCalcs():
@@ -167,13 +183,14 @@ def printCalcs():
 
     #get the max payout from poolpicker
     ppmax = getPoolPickerAlgo(ppalgo)
+    # you can replace getPoolPicker with your own function that returns the highest paying pool price
 
     # the getmrrlow takes 2 arguments
     #  number of lowest prices to ignore
     #  poolpicker max payout
     mrrlow = getmrrlow(3,ppmax)
 
-    print "PoolPicker Max: " + str(ppmax)
+    #print "PoolPicker Max: " + str(ppmax)
     print "MRR Lowest    : " + str(mrrlow)
     #The following command triggers all of the work.
     # There are four main argument to updateMyRigsPrices
@@ -196,3 +213,4 @@ def printCalcs():
 
 if __name__ == '__main__':
     printCalcs()
+    #read through printCalcs for explanation and where else to read
