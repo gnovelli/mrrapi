119b1a37a482c05d664f736d4e7aabd405ddab4e
JC Woltz <jcwoltz@ocsm.local.jcwoltz.com>
2014-08-14 15:17:50 -0400

    make setuptools work

--- setup.py
+++ setup.py
@@ -1,34 +1,30 @@
 import os
 
 try:
-    from setuptools import setup, find_packages
+    from setuptools import setup, find_packages # Always prefer setuptools over distutils
 except ImportError:
     from distutils.core import setup
 
 here = os.path.abspath(os.path.dirname(__file__))
-README = open(os.path.join(here, 'README.md')).read()
-CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
-
-requires = ['requests']
+# Get the long description from the relevant file
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
     name='mrrapi',
-    version='0.3',
+    version='0.3.1',
     url='https://github.com/jcwoltz/mrrapi',
     description='MinigRigRentals.com python API integration',
-    long_description=README + '\n\n' + CHANGES,
+    long_description=read('README.md') + '\n\n' + read('CHANGES.txt'),
     classifiers=[
+        "Development Status :: 4 - Beta",
         "Programming Language :: Python",
         "Topic :: Office/Business :: Financial"
     ],
     author='jcwoltz',
     author_email='jwoltz@gmail.com',
     keywords='mrr miningrigrentals api bitcoin',
-    packages=['mrrapi'],
+    packages=find_packages(exclude=['contrib', 'example*', 'docs', 'tests*']),
     install_requires=['requests'],
-    include_package_data=True,
-    zip_safe=False,
-    install_requires=requires,
-    tests_require=requires,
     test_suite="mrrapi",
 )
