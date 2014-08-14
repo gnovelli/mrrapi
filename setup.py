df34b6acfe54f37dd86d5b46d22c2bc76be9ff69
J.C. Woltz <jwoltz@gmail.com>
2014-08-14 10:24:51 -0400

    preparing proper setup

--- setup.py
+++ setup.py
@@ -9,8 +9,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.md')).read()
 CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
 
-requires = [
-]
+requires = ['requests']
 
 setup(
     name='mrrapi',
