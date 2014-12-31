d1fbf9c278bf584a8708a8c98c743e678b667168
J.C. Woltz <jwoltz@gmail.com>
2014-12-31 16:18:48 -0500

    test terminal size

--- scripts/list_myrigs.py
+++ scripts/list_myrigs.py
@@ -26,6 +26,34 @@ def lineno():
     cf = currentframe()
     return cf.f_back.f_lineno
 
+def getTerminalSize():
+    import os
+    env = os.environ
+    def ioctl_GWINSZ(fd):
+        try:
+            import fcntl, termios, struct, os
+            cr = struct.unpack('hh', fcntl.ioctl(fd, termios.TIOCGWINSZ,
+        '1234'))
+        except:
+            return
+        return cr
+    cr = ioctl_GWINSZ(0) or ioctl_GWINSZ(1) or ioctl_GWINSZ(2)
+    if not cr:
+        try:
+            fd = os.open(os.ctermid(), os.O_RDONLY)
+            cr = ioctl_GWINSZ(fd)
+            os.close(fd)
+        except:
+            pass
+    if not cr:
+        cr = (env.get('LINES', 25), env.get('COLUMNS', 80))
+
+        ### Use get(key[, default]) instead of a try/catch
+        #try:
+        #    cr = (env['LINES'], env['COLUMNS'])
+        #except:
+        #    cr = (25, 80)
+    return int(cr[1]), int(cr[0])
 
 mapi = mrrapi.api(mkey,msecret)
 debug = False
@@ -141,6 +169,7 @@ def main():
     if options.debug:
         debug = True
         print options
+        print getTerminalSize()
 
     myrigs = mapi.myrigs()
     if myrigs['success'] is not True:
