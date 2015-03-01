f7932883b3792347d4afa46db3eaa898f0581aaf
J.C. Woltz <jwoltz@gmail.com>
2015-03-01 12:28:31 -0500

    use helpers and reformat

--- mrrapi/tools/list_myrigs.py
+++ mrrapi/tools/list_myrigs.py
@@ -1,3 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+    This script will list all of your rig at MiningRigRentals.com except for the rigs that have
+    retired or test in their name. It will display that max income for a 24 hour period after MRR
+    fee and withdrawl fee.
+"""
 import json
 import urllib2
 import ConfigParser
@@ -11,9 +18,11 @@ import mrrapi.helpers
 debug = False
 ___version___ = '0.0.1'
 
+
 def lineno():
     cf = currentframe()
     return cf.f_back.f_lineno
+
 
 def calculateMaxIncomeAlgo(parsedrigs):
     global mhash
@@ -30,10 +39,26 @@ def calculateMaxIncomeAlgo(parsedrigs):
             nametmp = len(parsedrigs[algo][x])
             if nametmp > namelen:
                 namelen = nametmp
-            #print x, algo, namelen
+                # print x, algo, namelen
 
-    layout = "{0:>" + str(namelen) + "}{1:>10}{2:>10}{3:>13}{4:>17}{5:>14}{6:>14}{7:>10}"
-    print(layout.format("  Device Name  ", " Type ", " Speed ","Cur hash 30m","Price  ", "Daily income", "Rented? ","RentID"))
+    lrigname = namelen
+    lrigtype = 10
+    lspeed = 10
+    lcurhash = 13
+    lprice = 17
+    ldaily = 14
+    lrented = 14
+    lrental = 10
+
+    ltotal = lrigname + lrigtype + lspeed + lcurhash + lprice + ldaily + lrented + lrental
+    if debug:
+        print lrigname
+        print ltotal
+        print mrrapi.helpers.getTerminalSize()
+    layout = "{0:>%s}{1:>%s}{2:>%s}{3:>%s}{4:>%s}{5:>%s}{6:>%s}{7:>%s}" % (
+    str(lrigname), str(lrigtype), str(lspeed), str(lcurhash), str(lprice), str(ldaily), str(lrented), str(lrental))
+    print(layout.format("  Device Name  ", " Type ", " Speed ", "Cur hash 30m", "Price  ", "Daily income", "Status  ",
+                        "RentID"))
 
     for algo in parsedrigs:
         algorigs = parsedrigs[algo]
@@ -45,11 +70,11 @@ def calculateMaxIncomeAlgo(parsedrigs):
             rigstat = "available"
             curhash = float(0.0)
             rentid = ''
-            mhashrate = float(t['hashrate']['advertised'])/(1000000.0)
+            mhashrate = float(t['hashrate']['advertised']) / (1000000.0)
             mhash += mhashrate
-            admhashrate = nicehash(float(t['hashrate']['advertised'])/(1000000.0))
+            admhashrate = nicehash(float(t['hashrate']['advertised']) / (1000000.0))
             dailyprice = mhashrate * float(t['price']) * (1.0 - rentalfee)
-            curhash = nicehash(round(float(t['hashrate']['30min'])/10**6,3))
+            curhash = nicehash(round(float(t['hashrate']['30min']) / 10 ** 6, 3))
             if (str(t['status']) == 'rented'):
                 aih = float(t['available_in_hours'])
                 rigstat = "R "
@@ -61,23 +86,26 @@ def calculateMaxIncomeAlgo(parsedrigs):
                 rigstat = "disabled"
                 outcome -= dailyprice
 
-            print(layout.format(str(t['name']),str(t['type']),str(admhashrate),str(curhash),mrrapi.helpers.ff12(float(t['price'])) ,mrrapi.helpers.ff(dailyprice), rigstat,rentid))
+            print(layout.format(str(t['name']), str(t['type']), str(admhashrate), str(curhash),
+                                mrrapi.helpers.ff12(float(t['price'])), mrrapi.helpers.ff(dailyprice), rigstat, rentid))
             outcome += dailyprice
 
     return outcome
+
 
 def nicehash(mhashrate):
     mhunit = "MH"
     if 1000 <= mhashrate < 1000000:
         mhunit = "GH"
-        mhashrate = round(float(mhashrate/1000),3)
+        mhashrate = round(float(mhashrate / 1000), 3)
     elif mhashrate >= 1000000:
         mhunit = "TH"
-        mhashrate = round(float(mhashrate/1000000),3)
+        mhashrate = round(float(mhashrate / 1000000), 3)
     elif mhashrate >= 1000000000:
         mhunit = "PH"
-        mhashrate = round(float(mhashrate/1000000000),3)
+        mhashrate = round(float(mhashrate / 1000000000), 3)
     return (str(mhashrate) + " " + mhunit)
+
 
 def main():
     global debug, mapi
@@ -90,25 +118,37 @@ def main():
         print options
         print mrrapi.helpers.getTerminalSize()
 
-    (mkey, msecret) = mrrapi.helpers.getmrrconfig()
-    mapi = mrrapi.api(mkey,msecret)
-
+    try:
+        (mkey, msecret) = mrrapi.helpers.getmrrconfig()
+        mapi = mrrapi.api(mkey, msecret)
+    except:
+        print "ERROR: Unable to find or parse mrrapi.cfg"
+        sys.exit(2)
 
     myrigs = mapi.myrigs()
     if myrigs['success'] is not True:
         print "Error getting my rig listings"
         if str(myrigs['message']) == 'not authenticated':
             print 'Make sure you fill in your key and secret that you get from https://www.miningrigrentals.com/account/apikey'
+        print myrigs
+        sys.exit(3)
     else:
-        prigs = mrrapi.helpers.parsemyrigs(myrigs,True)
-        #print prigs
+        prigs = mrrapi.helpers.parsemyrigs(myrigs, True)
+        # print prigs
+        print ""
+
         maxi = calculateMaxIncomeAlgo(prigs)
         bal = mapi.getbalance()
-        btcv = mrrapi.helpers.getBTCValue()
+        try:
+            btcv = mrrapi.helpers.getBTCValue()
+        except:
+            btcv = float(1.1)
         print
-        print "Max income/day : %s BTC. USD: %s" % (str(round(maxi,8) - 0.002),str(round(btcv*(maxi -0.002),2)))
-        print "Current Balance: %s BTC. USD: %s" % (str(bal['data']['confirmed']),str(round(btcv*float(bal['data']['confirmed']),2)))
-        print "Pending Balance: %s BTC. USD: %s" % (str(bal['data']['unconfirmed']),str(round(btcv*float(bal['data']['unconfirmed']),2)))
+        print " Max income/day : %s BTC. USD: $%s" % (str(round(maxi, 8) - 0.002), str(round(btcv * (maxi - 0.002), 2)))
+        print "Current Balance : %s BTC. USD: $%s" % (
+        str(bal['data']['confirmed']), str(round(btcv * float(bal['data']['confirmed']), 2)))
+        print "Pending Balance : %s BTC. USD: $%s" % (
+        str(bal['data']['unconfirmed']), str(round(btcv * float(bal['data']['unconfirmed']), 2)))
 
 
 if __name__ == '__main__':
